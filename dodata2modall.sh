e6f9df5dfbc32b2e1e480bf38e8c8b705eab70e2
roccorollo <a.rossi@iasfbo.inaf.it>
2018-03-13 12:11:40 +0100

    forgot lc from photometry, new lc

--- dodata2modall.sh
+++ dodata2modall.sh
@@ -7,6 +7,10 @@ do  name=$(basename $i) ;
 	python data2mod.py $i superkn170817model_43p66mpc_${name} 0.0098;
 done
 
+# backup old
+cp -r /home/rossi/work/kilonova/kilonovamodels/gw170817superspectra \
+           /home/rossi/work/kilonova/kilonovamodels/gw170817superspectra_old 
+
 #remove last 5 epochs from spectra:
 rm superkn170817model_43p66mpc_supernewXSGW0829_smooth.dat
 rm superkn170817model_43p66mpc_supernewXSGW0830_smooth.dat
@@ -17,15 +21,16 @@ rm superkn170817model_43p66mpc_supernewXSGW0902_smooth.dat
 # keep only photometry from 0.5,1 , 2.5 and 4.5 days
 mkdir mmm123456
 mv superkn170817model_43p66mpc_supersed_time579* mmm123456/
-mv mmm/superkn170817model_43p66mpc_supersed_time57983.03.dat ./
-mv mmm/superkn170817model_43p66mpc_supersed_time57983.53.dat ./
-mv mmm/superkn170817model_43p66mpc_supersed_time57984.95.dat ./
-mv mmm/superkn170817model_43p66mpc_supersed_time57987.03.dat ./
+mv mmm123456/superkn170817model_43p66mpc_supersed_time57983.03.dat ./
+mv mmm123456/superkn170817model_43p66mpc_supersed_time57983.53.dat ./
+mv mmm123456/superkn170817model_43p66mpc_supersed_time57984.95.dat ./
+mv mmm123456/superkn170817model_43p66mpc_supersed_time57987.03.dat ./
 
 rm -rf mmm123456
 
-cp -r /home/rossi/work/kilonova/kilonovamodels/gw170817superspectra 
-cp -r /home/rossi/work/kilonova/kilonovamodels/gw170817superspectra_old 
+# remove old and put new
 rm gw170817superspectra/*
 mv superkn170817model_43p66mpc_* gw170817superspectra/
+
+
 
